480d267c7bbcbc1b138b66c484dc386926c6c8f5
Jona Strickler <jona.strickler@web.de>
2023-03-14 18:47:22 +0100

    corrected placeholder text

--- IF2023_Website/src/pages/Placeholder/Placeholder.jsx
+++ IF2023_Website/src/pages/Placeholder/Placeholder.jsx
@@ -1,23 +1,21 @@
 import styles from "./Placeholder.module.css";
 const Placeholder = () => {
-  return (
-    <div className={styles.gridContainer}>
-      <h1 className={styles.title}>
-        INTERACTIVE <br /> FUTURE <br /> EXHIBITION
-      </h1>
-      <img className={styles.logo} src="/assets/IF-Logo_white.svg" alt="Logo" />
-      <div className={styles.info}>
-        <p>14. - 16.6.2023</p>
-        <p>
-          806 m<sup>2</sup>
-        </p>
-        <br />
-        <p className={styles.commingSoon}>
-          Weiter Informationen folgen in Kürze
-        </p>
-      </div>
-    </div>
-  );
+	return (
+		<div className={styles.gridContainer}>
+			<h1 className={styles.title}>
+				INTERACTIVE <br /> FUTURE <br /> EXHIBITION
+			</h1>
+			<img className={styles.logo} src="/assets/IF-Logo_white.svg" alt="Logo" />
+			<div className={styles.info}>
+				<p>18. - 21.7.2023</p>
+				<p>806 qm</p>
+				<br />
+				<p className={styles.commingSoon}>
+					Weiter Informationen folgen in Kürze
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default Placeholder;
