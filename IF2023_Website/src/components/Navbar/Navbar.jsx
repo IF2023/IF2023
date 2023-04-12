625d0a46032fbd6b1f1994e043034029de1e5e44
Jona Strickler <jona.strickler@web.de>
2023-04-12 16:34:45 +0200

    feat:rough navbar styling

--- IF2023_Website/src/components/Navbar/Navbar.jsx
+++ IF2023_Website/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,14 @@
 import styles from "./Navbar.module.scss";
 
-
 const Navbar = () => {
-  return <nav className={styles.navbar}>Navbar</nav>;
+  return <nav className={styles.navbar}>
+    <img className={styles.navbar__logo} src="/public/assets/IF-Logo_white.svg" alt="IF Logo" />
+    <div className={styles.navbar__links}>
+      <p>Programm</p>
+      <p>Projekte</p>
+      <p>Electives</p>
+    </div>
+  </nav>;
 };
 
 export default Navbar;
