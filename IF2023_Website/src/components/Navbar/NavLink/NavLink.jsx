bd2cc5470d8ab9d67a17f95682f8ce6099407075
Jona Strickler <jona.strickler@web.de>
2023-04-14 20:22:28 +0200

    feat: navbar fully responsive

--- IF2023_Website/src/components/Navbar/NavLink/NavLink.jsx
+++ IF2023_Website/src/components/Navbar/NavLink/NavLink.jsx
@@ -1,9 +1,9 @@
 import styles from "./NavLink.module.scss";
 import { Link } from "react-router-dom";
 
-const NavLink = ({ name, destinationId }) => {
+const NavLink = ({ name, destinationId, navToggle }) => {
   return (
-    <Link className={styles.navLink} to={`/#${destinationId}`}>
+    <Link className={styles.navLink} to={`/#${destinationId}`} onClick={navToggle}>
       {name}
     </Link>
   );
