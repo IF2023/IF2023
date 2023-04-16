b9d4b02cedb61661f332ecd0dd8dec1591580985
Jona Strickler <jona.strickler@web.de>
2023-04-17 00:20:01 +0200

    feat: speaker section and single component

---
+++ IF2023_Website/src/components/SingleSpeaker/SingleSpeaker.jsx
@@ -0,0 +1,20 @@
+import AnimationWrapper from "../AnimationWrapper/AnimationWrapper";
+import styles from "./SingleSpeaker.module.scss";
+
+const SingleSpeaker = ({ data }) => {
+	const { img, name, company, description } = data;
+	return (
+		<AnimationWrapper>
+			<div className={styles.singleSpeaker}>
+				<div className={styles.speaker__imgContainer}>
+					<img className={styles.speaker__img} src="bla" alt={img} />
+				</div>
+				<p className={styles.speaker__name}>{name}</p>
+				<p className={styles.speaker__company}>{company}</p>
+				<p className={styles.speaker__description}>{description}</p>
+			</div>
+		</AnimationWrapper>
+	);
+};
+
+export default SingleSpeaker;
--- IF2023_Website/src/sections/Speaker/Speaker.jsx
+++ IF2023_Website/src/sections/Speaker/Speaker.jsx
@@ -1,5 +1,22 @@
+import styles from "./Speaker.module.scss";
+import AnimationWrapper from "../../components/AnimationWrapper/AnimationWrapper";
+import SingleSpeaker from "../../components/SingleSpeaker/SingleSpeaker";
+import data from "../../data/speakers.json";
+
 const Speaker = () => {
-  return <section>Speaker</section>;
+	return (
+		<section id="speaker" className={styles.speaker}>
+			<AnimationWrapper>
+				<h1 className={styles.heading}>SPEAKER</h1>
+			</AnimationWrapper>
+			<div className={styles.speaker__wrapper}>
+				{data.map((speaker) => {
+					// return <p>{speaker.name}</p>
+					return <SingleSpeaker key={speaker.id} data={speaker} />;
+				})}
+			</div>
+		</section>
+	);
 };
 
 export default Speaker;
