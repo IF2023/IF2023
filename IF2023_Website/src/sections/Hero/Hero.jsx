00fb6f8ab07032b623f2806b349ddf9f51156c49
Theresa Husske <theresa.husske@t-online.de>
2023-05-26 20:41:38 +0200

    feat: hero section for desktop

--- IF2023_Website/src/sections/Hero/Hero.jsx
+++ IF2023_Website/src/sections/Hero/Hero.jsx
@@ -1,7 +1,23 @@
 import styles from "./Hero.module.scss";
+import AnimationWrapper from "../../components/AnimationWrapper/AnimationWrapper";
 
 const Hero = () => {
-  return <section className={styles.hero}>Hero</section>;
+  return (
+    <section className={styles.hero}>
+      <div className={styles.IFinformation}>
+        <AnimationWrapper>
+          <h1 className={styles.heading}>
+            INTERACTIVE <br></br> FUTURE <br></br> EXHIBITION
+          </h1>
+        </AnimationWrapper>
+        <AnimationWrapper>
+          <p className={styles.information}>
+            18. - 21.7.2023 <br></br> 806 qm Darmstadt
+          </p>
+        </AnimationWrapper>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
