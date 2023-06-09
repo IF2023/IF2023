f4d4c4eec2e1b4868583f3cd044673948afb69a1
Olaf Yang <mail@olafyang.com>
2023-06-09 06:48:24 +0200

    initial works on sponsors section, see comment need revision on css background image, see comment of scss module

--- IF2023_Website/src/sections/Supporter/Supporter.jsx
+++ IF2023_Website/src/sections/Supporter/Supporter.jsx
@@ -1,5 +1,46 @@
+import AnimationWrapper from "../../components/AnimationWrapper/AnimationWrapper";
+import styles from "./Supporter.module.scss";
+
 const Supporter = () => {
-  return <section>Supporter</section>;
+  return (
+    <section className={styles.supporter}>
+      <AnimationWrapper>
+        <h1 className={styles.heading}>Danke an unsere Supporter!</h1>
+      </AnimationWrapper>
+      <div className={styles.grid}>
+        <div>
+          <div className={styles.big}>
+            <a href="">
+              <img src="https://placehold.co/760x260" alt="" />
+            </a>
+          </div>
+          <div className={styles.small}>
+            <a href="">
+              <img src="https://placehold.co/340x220" alt="" />
+            </a>
+            <a href="">
+              <img src="https://placehold.co/340x220" alt="" />
+            </a>
+          </div>
+        </div>
+        <div>
+          <div className={styles.big}>
+            <a href="">
+              <img src="https://placehold.co/760x260" alt="" />
+            </a>
+          </div>
+          <div className={styles.small}>
+            <a href="">
+              <img src="https://placehold.co/340x220" alt="" />
+            </a>
+            <a href="">
+              <img src="https://placehold.co/340x220" alt="" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Supporter;
