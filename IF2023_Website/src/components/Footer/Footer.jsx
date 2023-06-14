75600378f9e4ffebe93a5dcd6ab03c7511c9d4c5
Jona Strickler <jona.strickler@web.de>
2023-06-14 16:48:48 +0200

    social links with icons
    hover effects and responsiveness for socials are still missing

--- IF2023_Website/src/components/Footer/Footer.jsx
+++ IF2023_Website/src/components/Footer/Footer.jsx
@@ -1,21 +1,30 @@
-import styles from "./Footer.module.scss"
+import styles from "./Footer.module.scss";
+import { AiOutlineInstagram, AiOutlineGlobal } from "react-icons/ai";
 
 const Footer = () => {
-  return <footer className={styles.footer}>
-    <div className={styles.socials}>
-      {/* <img src="" alt="" />
-      <img src="" alt="" />
-      <img src="" alt="" /> */}
-      <div></div>
-      <div></div>
-      <div></div>
-    </div>
-    <div className={styles.pages}>
-      <a href="/impressum">Impressum</a>
-      <a href="/kontakt">Kontakt</a>
-      <a href="/datenschutz">Datenschutz</a>
-    </div>
-  </footer>;
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.socials}>
+        <a href="https://imd.mediencampus.h-da.de/">
+          <div className={styles.icon}>
+            <AiOutlineGlobal />
+          </div>
+          <p>IMD Website</p>
+        </a>
+        <a href="https://www.instagram.com/interactivemediadesign.hda/">
+          <div className={styles.icon}>
+            <AiOutlineInstagram />
+          </div>
+          <p>IMD Instagram</p>
+        </a>
+      </div>
+      <div className={styles.pages}>
+        <a href="/impressum">Impressum</a>
+        <a href="/kontakt">Kontakt</a>
+        <a href="/datenschutz">Datenschutz</a>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
