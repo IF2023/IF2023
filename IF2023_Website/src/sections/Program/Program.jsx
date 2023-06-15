ca4f86dc727963ff0531028991d07efc0b274e29
Leslie Joyce Hartmann <leslie230894@gmail.com>
2023-06-16 00:04:31 +0200

    Feat: added correct program heading

--- IF2023_Website/src/sections/Program/Program.jsx
+++ IF2023_Website/src/sections/Program/Program.jsx
@@ -1,7 +1,15 @@
+import AnimationWrapper from "../../components/AnimationWrapper/AnimationWrapper";
+import Slideshow from "../../components/ProgramDay/ProgramDay";
 import styles from "./program.module.scss";
 
 const Program = () => {
-  return <section>Program</section>;
+  return (
+    <section id="program" className={styles.program}>
+      <AnimationWrapper>
+        <h1 className={styles.heading}>PROGRAM</h1>
+      </AnimationWrapper>
+    </section>
+  );
 };
 
 export default Program;
