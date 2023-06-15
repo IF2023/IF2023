4802e82c0bbea434a164da384eecd151a5cec22b
Leslie Joyce Hartmann <leslie230894@gmail.com>
2023-06-16 00:40:16 +0200

    feat: reading and displaying data from json file

--- IF2023_Website/src/components/ProgramDay/ProgramDay.jsx
+++ IF2023_Website/src/components/ProgramDay/ProgramDay.jsx
@@ -1,8 +1,35 @@
+import React, { useEffect, useState } from "react";
 import data from "../../data/programData.json";
 import styles from "./ProgramDay.module.scss";
 
 const ProgramDay = () => {
-  return <h1 className={styles.heading}>TEST</h1>;
+  const [programData, setProgramData] = useState(null);
+
+  useEffect(() => {
+    setProgramData(data);
+  }, []);
+
+  if (!programData) {
+    // Data is still loading, show a loading indicator or return null
+    return null;
+  }
+
+  return (
+    <div>
+      {programData.map((day) => (
+        <div key={day.id}>
+          <h2>{day.date}</h2>
+          <ul>
+            {day.program.map((item) => (
+              <li key={item.time}>
+                <span>{item.time}</span> - <span>{item.title}</span>
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default ProgramDay;
