22c3a803c1b9358bca2b5784f9efead9a0d7f124
Jona Strickler <jona.strickler@web.de>
2023-06-16 11:05:16 +0200

    navbar links now scroll to sections using id

--- IF2023_Website/src/components/Navbar/NavLink/NavLink.jsx
+++ IF2023_Website/src/components/Navbar/NavLink/NavLink.jsx
@@ -2,10 +2,25 @@ import styles from "./NavLink.module.scss";
 import { Link } from "react-router-dom";
 
 const NavLink = ({ name, destinationId, navToggle }) => {
-  return (
-    <Link className={styles.navLink} to={`/#${destinationId}`} onClick={navToggle}>
-      {name}
-    </Link>
-  );
+	const scrollToSection = (destinationId) => {
+		const elemTop = document
+			.getElementById(destinationId)
+			.getBoundingClientRect().top;
+
+		// scroll to section position minus navbar height
+		window.scrollTo(0, elemTop - 120);
+	};
+
+	return (
+		<p
+			className={styles.navLink}
+			onClick={() => {
+				navToggle();
+				scrollToSection(destinationId);
+			}}
+		>
+			{name}
+		</p>
+	);
 };
 export default NavLink;
