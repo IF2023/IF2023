330fdd2abda635c22b6a75892f26422362289153
Jona Strickler <jona.strickler@web.de>
2023-06-16 23:00:20 +0200

    changed footer links to react-router links

--- IF2023_Website/src/components/Footer/Footer.jsx
+++ IF2023_Website/src/components/Footer/Footer.jsx
@@ -1,30 +1,52 @@
 import styles from "./Footer.module.scss";
 import { AiOutlineInstagram, AiOutlineGlobal } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  return (
-    <footer className={styles.footer}>
-      <div className={styles.socials}>
-        <a href="https://imd.mediencampus.h-da.de/">
-          <div className={styles.icon}>
-            <AiOutlineGlobal />
-          </div>
-          IMD Website
-        </a>
-        <a href="https://www.instagram.com/interactivemediadesign.hda/">
-          <div className={styles.icon}>
-            <AiOutlineInstagram />
-          </div>
-          IMD Instagram
-        </a>
-      </div>
-      <div className={styles.pages}>
-        <a href="/impressum">Impressum</a>
-        <a href="/kontakt">Kontakt</a>
-        <a href="/datenschutz">Datenschutz</a>
-      </div>
-    </footer>
-  );
+	return (
+		<footer className={styles.footer}>
+			<div className={styles.socials}>
+				<a href="https://imd.mediencampus.h-da.de/">
+					<div className={styles.icon}>
+						<AiOutlineGlobal />
+					</div>
+					IMD Website
+				</a>
+				<a href="https://www.instagram.com/interactivemediadesign.hda/">
+					<div className={styles.icon}>
+						<AiOutlineInstagram />
+					</div>
+					IMD Instagram
+				</a>
+			</div>
+			<div className={styles.pages}>
+				<Link
+					to="/impressum"
+					onClick={() =>
+						window.scrollTo({
+							top: 0,
+							left: 0,
+							behavior: "instant",
+						})
+					}
+				>
+					Impressum
+				</Link>
+				<Link
+					to="/datenschutz"
+					onClick={() =>
+						window.scrollTo({
+							top: 0,
+							left: 0,
+							behavior: "instant",
+						})
+					}
+				>
+					Datenschutz
+				</Link>
+			</div>
+		</footer>
+	);
 };
 
 export default Footer;
