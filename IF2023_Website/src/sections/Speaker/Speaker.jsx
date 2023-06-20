5aac4c09d4f20e6706c61a2c3af170774c16f86e
Jona Strickler <jona.strickler@web.de>
2023-06-20 14:15:41 +0200

    coming soon headings styling and responsiveness

--- IF2023_Website/src/sections/Speaker/Speaker.jsx
+++ IF2023_Website/src/sections/Speaker/Speaker.jsx
@@ -4,19 +4,23 @@ import SingleSpeaker from "../../components/SingleSpeaker/SingleSpeaker";
 import data from "../../data/speakers.json";
 
 const Speaker = () => {
-	return (
-		<section id="speaker" className={styles.speaker}>
-			<AnimationWrapper>
-				<h1 className={styles.heading}>SPEAKER</h1>
-			</AnimationWrapper>
-			<div className={styles.speaker__wrapper}>
+  return (
+    <section id="speaker" className={styles.speaker}>
+      <AnimationWrapper>
+        <h1 className={styles.heading}>SPEAKER</h1>
+      </AnimationWrapper>
+      {/* <div className={styles.speaker__wrapper}>
 				{data.map((speaker) => {
-					// return <p>{speaker.name}</p>
 					return <SingleSpeaker key={speaker.id} data={speaker} />;
 				})}
-			</div>
-		</section>
-	);
+			</div> */}
+      <div className={styles.comingSoonContainer}>
+        <AnimationWrapper>
+          <h3 className={styles.comingSoon}>COMING SOON</h3>
+        </AnimationWrapper>
+      </div>
+    </section>
+  );
 };
 
 export default Speaker;
